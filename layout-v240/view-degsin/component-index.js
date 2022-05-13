7572cea60199e1b724a0ba667a9d3aebbc78ef95
imycod <317009160@qq.com>
2022-05-13 09:11:11 +0800

    feat: 复制

---
+++ layout-v240/view-degsin/component-index.js
@@ -0,0 +1,417 @@
+// import path from "path";
+
+const Hamburger = {
+  name: 'Hamburger',
+  template: `
+    <div style="padding: 0 15px;" @click="toggleClick">
+    <svg
+        :class="{'is-active':isActive}"
+        class="hamburger"
+        viewBox="0 0 1024 1024"
+        xmlns="http://www.w3.org/2000/svg"
+        width="64"
+        height="64"
+    >
+      <path
+          d="M408 442h480c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8H408c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8zm-8 204c0 4.4 3.6 8 8 8h480c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8H408c-4.4 0-8 3.6-8 8v56zm504-486H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 632H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zM142.4 642.1L298.7 519a8.84 8.84 0 0 0 0-13.9L142.4 381.9c-5.8-4.6-14.4-.5-14.4 6.9v246.3a8.9 8.9 0 0 0 14.4 7z"/>
+    </svg>
+    </div>
+  `,
+  props: {
+    isActive: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    toggleClick() {
+      this.$emit('toggleClick');
+    }
+  }
+};
+const {mapGetters} = Vuex;
+const Navbar = {
+  template: `
+    <div class="navbar">
+    <hamburger :is-active="sidebar.opened" class="hamburger-container" @toggleClick="toggleSideBar"/>
+      <div class="right-menu">
+<!--        <el-dropdown class="avatar-container" trigger="click">-->
+<!--          <div class="avatar-wrapper">-->
+<!--            <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">-->
+<!--            <i class="el-icon-caret-bottom"/>-->
+<!--          </div>-->
+<!--          <el-dropdown-menu slot="dropdown" class="user-dropdown">-->
+<!--            <router-link to="/">-->
+<!--              <el-dropdown-item>-->
+<!--                Home-->
+<!--              </el-dropdown-item>-->
+<!--            </router-link>-->
+<!--            <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">-->
+<!--              <el-dropdown-item>Github</el-dropdown-item>-->
+<!--            </a>-->
+<!--            <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">-->
+<!--              <el-dropdown-item>Docs</el-dropdown-item>-->
+<!--            </a>-->
+<!--            <el-dropdown-item divided @click.native="logout">-->
+<!--              <span style="display:block;">Log Out</span>-->
+<!--            </el-dropdown-item>-->
+<!--          </el-dropdown-menu>-->
+<!--        </el-dropdown>-->
+      </div>
+    </div>
+  `,
+  components: {
+    Hamburger
+  },
+  computed: {
+    ...mapGetters([
+      'sidebar',
+      'avatar'
+    ])
+  },
+  methods: {
+    toggleSideBar() {
+      this.$store.dispatch('app/toggleSideBar');
+    },
+    async logout() {
+      await this.$store.dispatch('user/logout');
+      this.$router.push(`/login?redirect=${this.$route.fullPath}`);
+    }
+  }
+};
+
+const useIconVnode = (h, tag, props) => {
+  let aray = [];
+  const elHtml = h(tag, props);
+  aray.push(elHtml);
+  return aray;
+};
+
+const Item = {
+  name: 'MenuItem',
+  functional: true,
+  props: {
+    icon: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    }
+  },
+  render(h, context) {
+    const {icon, title} = context.props;
+    const vnodes = [];
+    if (icon) {
+      if (icon.includes('el-icon')) {
+        const elHtml = h('i', {
+          class: [icon, 'sub-el-icon']
+        });
+        vnodes.push(elHtml);
+      } else {
+        if (isImg(icon)) {
+          const elHtml = h('img', {
+            class: ['icon-img'],
+            attrs: {
+              src: icon
+            }
+          });
+          vnodes.push(elHtml);
+        } else {
+          const elHtml = h('svg-icon', {
+            attrs: {
+              'icon-class': icon
+            }
+          });
+          vnodes.push(elHtml);
+        }
+        // https://blog.csdn.net/u010598111/article/details/102932175
+        // vnodes.push(<svg-icon icon-class={icon}/>)
+      }
+    }
+
+    if (title) {
+      const elHtml = h('span', {
+        attrs: {
+          slot: title,
+          // class:'system-menu-title'
+        }
+      }, title);
+      vnodes.push(elHtml);
+      // vnodes.push(<span slot="title">{(title)}</span>);
+    }
+    return vnodes;
+  }
+};
+const AppLink = {
+  template: `
+    <component :is="type" v-bind="linkProps(to)">
+    <slot/>
+    </component>
+  `,
+  props: {
+    to: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    isExternal() {
+      return isExternal(this.to);
+    },
+    type() {
+      if (this.isExternal) {
+        return 'a';
+      }
+      return 'router-link';
+    }
+  },
+  methods: {
+    linkProps(to) {
+      if (this.isExternal) {
+        return {
+          href: to,
+          target: '_blank',
+          rel: 'noopener'
+        };
+      }
+      return {
+        to: to
+      };
+    }
+  }
+};
+
+// import path from 'path'
+const SidebarItem = {
+  template: `
+    <div v-if="!item.hidden">
+    <template
+        v-if="hasOneShowingChild(item.children,item) && (!onlyOneChild.children||onlyOneChild.noShowingChildren)&&!item.alwaysShow">
+      <app-link v-if="onlyOneChild.meta" :to="resolvePath(onlyOneChild.path)">
+        <el-menu-item :index="resolvePath(onlyOneChild.path)" :class="{'submenu-title-noDropdown':!isNest}">
+          <item :icon="onlyOneChild.meta.icon||(item.meta&&item.meta.icon)" :title="onlyOneChild.meta.title"/>
+        </el-menu-item>
+      </app-link>
+    </template>
+
+    <el-submenu v-else ref="subMenu" :index="resolvePath(item.path)" popper-append-to-body>
+      <template slot="title">
+        <item v-if="item.meta" :icon="item.meta && item.meta.icon" :title="item.meta.title"/>
+      </template>
+      <sidebar-item
+          v-for="child in item.children"
+          :key="child.path"
+          :is-nest="true"
+          :item="child"
+          :base-path="resolvePath(child.path)"
+          class="nest-menu"
+      />
+    </el-submenu>
+    </div>
+  `,
+  name: 'SidebarItem',
+  components: {Item, AppLink},
+  mixins: [FixiOSBug],
+  props: {
+    // route object
+    item: {
+      type: Object,
+      required: true
+    },
+    isNest: {
+      type: Boolean,
+      default: false
+    },
+    basePath: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    // To fix https://github.com/PanJiaChen/vue-admin-template/issues/237
+    // TODO: refactor with render function
+    this.onlyOneChild = null;
+    return {};
+  },
+  methods: {
+    hasOneShowingChild(children = [], parent) {
+      const showingChildren = children.filter(item => {
+        if (item.hidden) {
+          return false;
+        } else {
+          // Temp set(will be used if only has one showing child)
+          this.onlyOneChild = item;
+          return true;
+        }
+      });
+
+      // When there is only one child router, the child router is displayed by default
+      if (showingChildren.length === 1) {
+        return true;
+      }
+
+      // Show parent if there are no child router to display
+      if (showingChildren.length === 0) {
+        this.onlyOneChild = {...parent, path: '', noShowingChildren: true};
+        return true;
+      }
+
+      return false;
+    },
+    resolvePath(routePath) {
+      if (isExternal(routePath)) {
+        return routePath;
+      }
+      if (isExternal(this.basePath)) {
+        return this.basePath;
+      }
+      return this.basePath + '/' + routePath;
+    }
+  }
+};
+const Logo = {
+  template: `
+    <div class="sidebar-logo-container" :class="{'collapse':collapse}">
+    <transition name="sidebarLogoFade">
+      <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
+        <img v-if="logo" :src="logo" class="sidebar-logo">
+        <h1 v-else class="sidebar-title">{{ title }} </h1>
+      </router-link>
+      <router-link v-else key="expand" class="sidebar-logo-link" to="/">
+        <img v-if="logo" :src="logo" class="sidebar-logo">
+        <h1 class="sidebar-title">{{ title }} </h1>
+      </router-link>
+    </transition>
+    </div>
+  `,
+  name: 'SidebarLogo',
+  props: {
+    collapse: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data() {
+    return {
+      title: 'Vue Admin Template',
+      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+    };
+  }
+};
+const Sidebar = {
+  template: `
+    <div :class="{'has-logo':showLogo}">
+    <logo v-if="showLogo" :collapse="isCollapse"/>
+    <el-scrollbar wrap-class="scrollbar-wrapper">
+      <el-menu
+          :default-active="activeMenu"
+          :collapse="isCollapse"
+          :background-color="variables.menuBg"
+          :text-color="variables.menuText"
+          :unique-opened="false"
+          :active-text-color="variables.menuActiveText"
+          :collapse-transition="false"
+          mode="vertical"
+      >
+        <sidebar-item v-for="route in routes" :key="route.path" :item="route" :base-path="route.path"/>
+      </el-menu>
+    </el-scrollbar>
+    </div>
+  `,
+  components: {SidebarItem, Logo},
+  computed: {
+    ...mapGetters([
+      'sidebar'
+    ]),
+    routes() {
+      console.log(' this.$router.options.routes', this.$router.options.routes);
+      return this.$router.options.routes;
+    },
+    activeMenu() {
+      const route = this.$route;
+      const {meta, path} = route;
+      // if set path, the sidebar will highlight the path you set
+      if (meta.activeMenu) {
+        return meta.activeMenu;
+      }
+      return path;
+    },
+    showLogo() {
+      return this.$store.state.settings.sidebarLogo;
+    },
+    variables() {
+      return {
+        menuBg: '#1f2d3d',
+        menuText: '#bfcbd9',
+        menuActiveText: '#f4f4f5',
+      };
+    },
+    isCollapse() {
+      return !this.sidebar.opened;
+    }
+  }
+};
+
+
+const AppMain = {
+  template: `
+    <section class="app-main">
+      <transition name="fade-transform" mode="out-in">
+        <router-view :key="key"/>
+      </transition>
+    </section>
+  `,
+  name: 'AppMain',
+  computed: {
+    key() {
+      return this.$route.path;
+    }
+  }
+};
+
+const layout = {
+  template: `
+    <div :class="classObj" class="app-wrapper">
+      <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside"/>
+      <sidebar class="sidebar-container"/>
+      <div class="main-container">
+        <div :class="{'fixed-header':fixedHeader}">
+          <navbar/>
+        </div>
+        <app-main/>
+      </div>
+    </div>
+  `,
+  components: {
+    Navbar,
+    Sidebar,
+    AppMain
+  },
+  mixins: [ResizeMixin],
+  computed: {
+    sidebar() {
+      return this.$store.state.app.sidebar;
+    },
+    device() {
+      return this.$store.state.app.device;
+    },
+    fixedHeader() {
+      return this.$store.state.settings.fixedHeader;
+    },
+    classObj() {
+      return {
+        hideSidebar: !this.sidebar.opened,
+        openSidebar: this.sidebar.opened,
+        withoutAnimation: this.sidebar.withoutAnimation,
+        mobile: this.device === 'mobile'
+      };
+    }
+  },
+  methods: {
+    handleClickOutside() {
+      this.$store.dispatch('app/closeSideBar', {withoutAnimation: false});
+    }
+  },
+};
